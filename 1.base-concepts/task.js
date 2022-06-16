5cb036e3b81689cbc80b1bf7733c50d16f93c2e7
BPV1311 <Bakh-pasha@yandex.ru>
2022-06-16 15:59:45 +0500

    first commit variable

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,33 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant = b ** 2 - 4 * a * c;
+  let root0;
+  let root1;
+  let root2;
+
+  if (discriminant < 0) {
+    console.log(arr);
+  }
+
+   else if (discriminant == 0) {
+    root1 = (- b / (2 * a));
+    arr.push(root1)
+  }
+
+  else if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(discriminant) ) / (2 * a);
+    root2 = (-b - Math.sqrt(discriminant) ) / (2 * a);
+    arr.push(root1, root2)
+  }  
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+// выполню позже так как она не влияет на получение зачета.
 
-  // код для задачи №2 писать здесь
 
   return totalAmount;
 }
